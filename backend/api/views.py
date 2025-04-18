08e4e85bebf54cee4c7cb04d501c878739865f50
naveen-ihub <naveen.s.ihub@snsgroups.com>
2025-04-18 15:05:59 +0530

    ok

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,9 +5,9 @@ from rest_framework import status
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-collection = db["mycollection"]
+client = MongoClient("mongodb+srv://placement_tracker:placement_tracker@cluster0.hrxkgqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["FormDatabase"]
+collection = db["FormCollection"]
 
 
 # Create your views here.
@@ -24,6 +24,29 @@ def get_data(request):
 @api_view(['POST'])
 def add_data(request):
     data = request.data
+#     data={
+#   "date": "2025-04-18T08:43:16.108Z",
+#   "reportedBy": "dsfsdfs",
+#   "snsceOffersReceived": "dsf",
+#   "snsctOffersReceived": "dfsdf",
+#   "unplacedSNSCE": "sdfs",
+#   "unplacedSNSCT": "dfsdf",
+#   "awaitedResults": "sdfsdfs",
+#   "offersTodayInternships": "dfsdf",
+#   "totalSinceAprilInternships": "dsfsd",
+#   "internshipRemarks": "fsdfsd",
+#   "offersTodayHighSalary": "fd",
+#   "totalSinceAprilHighSalary": "sfdsf",
+#   "highSalaryRemarks": "sdfsdfs",
+#   "internshipUpdates": [
+#     {
+#       "company": "dfsdf",
+#       "department": "sdf",
+#       "numberOfStudents": "sdf",
+#       "status": "sdf"
+#     }
+#   ]
+# }
     collection.insert_one(data)
     return Response({"status": "Data added"}, status=status.HTTP_201_CREATED)
 
